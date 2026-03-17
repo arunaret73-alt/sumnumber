41a11699a3431fb9e00465eaef2c609977235188
arunaret73-alt <arunaret@gmial.com>
2026-03-17 20:22:48 +0700

    update notice

--- sumnumber.js
+++ sumnumber.js
@@ -1,5 +1,9 @@
-const sum = (a,b,c,d) => a + b + c + d;
+// const sum = (a,b,c,d) => a + b + c + d;
 
-console.log(sum(3,2,4,5));
+// console.log(sum(3,2,4,5));
 
-sum();
+// sum();
+
+const deduct = (x,y) => x - y
+console.log(deduct(5,7));
+deduct();
